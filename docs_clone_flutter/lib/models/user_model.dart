06d7493ec945aa4d23a7ca44a86dda5cda95704f
rubayet211 <rubayet211@gmail.com>
2023-07-28 00:29:40 +0600

    4th Commit

---
+++ docs_clone_flutter/lib/models/user_model.dart
@@ -0,0 +1,14 @@
+class UserModel {
+  final String email;
+  final String name;
+  final String profilePic;
+  final String uid;
+  final String token;
+  UserModel({
+    required this.email,
+    required this.name,
+    required this.profilePic,
+    required this.uid,
+    required this.token,
+  });
+}
--- docs_clone_flutter/lib/repository/auth_repository.dart
+++ docs_clone_flutter/lib/repository/auth_repository.dart
@@ -1,3 +1,7 @@
+import 'dart:html';
+
+import 'package:docs_clone_flutter/constants.dart';
+import 'package:docs_clone_flutter/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -6,17 +10,34 @@ final authRepositoryProvider =
 
 class AuthRepository {
   final GoogleSignIn _googleSignIn;
+  final Client _client;
   AuthRepository({
     required GoogleSignIn googleSignIn,
+    required Client client,
   }) : _googleSignIn = googleSignIn;
+       _client = client;
 
   void signInWithGoogle() async {
     try {
       final user = await _googleSignIn.signIn();
       if (user != null) {
-        print(user.email);
-        print(user.displayName);
-        print(user.photoUrl);
+        final userAcc = UserModel(
+          email: user.email,
+          name: user.displayName!,
+          profilePic: user.photoUrl!,
+          uid: '',
+          token: '',
+        );
+        var res = await _client.post(Uri.parse('$host/api/signup'),
+           body: userAcc.toJson(),
+           header: {
+              'Content-Type': 'application/json; charset=UTF-8',
+
+           });
+
+           switch(res.statusCode){
+            
+           }
       }
     } catch (e) {
       print(e);
